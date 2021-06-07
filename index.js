6d0171bf95d12e54930afc1c729fdc38e8fe42bf
juani <juanigm99@gmail.com>
2021-06-07 17:43:25 -0300

    Intento login

--- index.js
+++ index.js
@@ -20,14 +20,13 @@ type Producto{
 
 type Cliente{
 
-    id: ID
-    nombre: String
-    apellido: String
-    documento: Int
+    ClientID: ID
+    Nombre: String
+    Ppellido: String
+    Documento: Int
     mail: String
     password: String
-    puntos: Int
-    productosComprados: [Producto]
+    Puntos: Int
 }
 
 
@@ -56,15 +55,16 @@ type User {
     productos: [Producto],
     clientes: [Cliente],
     categorias: [Categoria],
+    login(mail: String, password: String): [Cliente]
     getUsers: [User],
     getUserInfo(id: Int) : User
   }
 
   type Mutation {
     addProducto(precio: Float, marca: String, descripcion: String, categoria: Int): Boolean
-    addCategoria(nombre: String, descripcion: String): Boolean
-    addCliente(nombre: String, apellido: String, documento: Int, mail: String, puntos: Int): Categoria
-    login(mail: String, password: String): Boolean
+    addCategoria(nombre: String, descripcion: String): Categoria
+    addCliente(Nombre: String, Apellido: String, Documento: Int, mail: String, password: String, Puntos: Int): Boolean
+    
     updateUserInfo(id: Int, name: String, email: String, job_title: String) : Boolean
     createUser(name: String, email: String, job_title: String) : Boolean
     deleteUser(id: Int) : Boolean
@@ -81,38 +81,56 @@ const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
 });
 
 const root = {
-  productos: (args, req) => queryDB(req, "select * from producto").then(data => data),
+  Query:{
+
+  productos: (args, req) => queryDB(req, "select * from producto").then((data) => {console.log(data); return data;}),
   addProducto: (args, req) => queryDB(req, "insert into producto SET ?", args).then((data) => {console.log(data); return data;}),
   clientes: (args, req) => queryDB(req, "select * from cliente").then(data => data),
   addCliente: (args, req) => queryDB(req, "insert into cliente SET ?", args).then((data) => {console.log(data); return data;}),
-  login: (args, req) => queryDB(req, "select from cliente SET ? where mail = ? && contraseña = ?", [args.mail, args.contraseña]).then((data) => {console.log(data); return data;}),
-  categorias: (args, req) => queryDB(req, "select * from categorias").then(data => data),
 
-  addCategoria: (args, req) => {
-    queryDB(req, "insert into categoria SET ?", args)
-    .then((data) => {
-      const cat = JSON.parse(JSON.stringify(data));
-      console.log('data1: ', cat);
-      queryDB(req, "select * from categoria where categoriaID = " + cat.insertId)
+  login: (args, req) => {
+     queryDB(req, "select * from cliente where mail = ? and password = ?", [args.mail, args.password]).then(data => data)
+     //.then((data) => {
+       //console.log(data);
+        //return true;
+      //})
+    },
+  categorias: (args, req) => queryDB(req, "select * from categorias").then(data => data)
+  },
+  Mutation: {
+    addCategoria: (args, req) => {
+      queryDB(req, "insert into categoria SET ?", args)
+      //.then(data => data);
+  
+  
       .then((data) => {
-        //const result = Object.values(JSON.parse(JSON.stringify(data[0])));
-        //console.log('result', result);
-        console.log('data', JSON.parse(JSON.stringify(data)));
-        console.log('data[0]', JSON.parse(JSON.stringify(data[0])));
-        const result = JSON.parse(JSON.stringify(data[0]));
-        return result;
-
-        //console.log('data[0][0]: ', data[0][0]);
-        //return data[0][0];
+        const cat = JSON.parse(JSON.stringify(data));
+        return cat.insertId;
+      }).then((res) => {
+        queryDB(req, "select * from categoria where categoriaID = " + cat.insertId)
+  
+        .then((data) => {
+  
+          //const result = Object.values(JSON.parse(JSON.stringify(data[0])));
+          //console.log('result', result);
+          console.log('data', JSON.parse(JSON.stringify(data)));
+          console.log('data[0]', JSON.parse(JSON.stringify(data[0])));
+          const result = JSON.parse(JSON.stringify(data[0]));
+          return {result};
+  
+          //console.log('data[0][0]: ', data[0][0]);
+          //return data[0][0];
+        })
+  
       })
-    })
+    }
   },
 
-  getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
+  /*getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
   getUserInfo: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0]),
   updateUserInfo: (args, req) => queryDB(req, "update users SET ? where id = ?", [args, args.id]).then(data => data),
   createUser: (args, req) => queryDB(req, "insert into users SET ?", args).then(data => data),
-  deleteUser: (args, req) => queryDB(req, "delete from users where id = ?", [args.id]).then(data => data)
+  deleteUser: (args, req) => queryDB(req, "delete from users where id = ?", [args.id]).then(data => data)*/
 };
 
 app.use((req, res, next) => {
