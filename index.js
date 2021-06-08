cebd5622c46b4598e98f17491e47288a54e691d9
juani <juanigm99@gmail.com>
2021-06-08 15:20:43 -0300

    Canjes manuales

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ type Cliente{
 
     ClientID: ID
     Nombre: String
-    Ppellido: String
+    Apellido: String
     Documento: Int
     mail: String
     password: String
@@ -36,12 +36,18 @@ type Categoria{
     descripcion: String
 }
 
-type Canje{
+scalar Date
 
-  id: ID
-  ProductoID: Int
-  ClienteID: Int
-  TotalCompra: Float
+type Canje{
+  canjeID: Int
+  clientID: Int
+  fecha: Date
+  totalPuntos: Int
+}
+
+type canjeproducto{
+  canjeID: Int
+  productID: Int
 }
 
 type User {
@@ -55,7 +61,8 @@ type User {
     productos: [Producto],
     clientes: [Cliente],
     categorias: [Categoria],
-    login(mail: String, password: String): [Cliente]
+    login(mail: String, password: String): [Cliente],
+    canjes: [Canje],
     getUsers: [User],
     getUserInfo(id: Int) : User
   }
@@ -64,7 +71,8 @@ type User {
     addProducto(precio: Float, marca: String, descripcion: String, categoria: Int): Boolean
     addCategoria(nombre: String, descripcion: String): Categoria
     addCliente(Nombre: String, Apellido: String, Documento: Int, mail: String, password: String, Puntos: Int): Boolean
-    
+    addCanje(clientID: Int, fecha: Date, totalPuntos: Int, token: String, productos: [Producto]): [Canje]
+    addCanjeProducto(canjeID: Int, productID: Int): [canjeproducto]
     updateUserInfo(id: Int, name: String, email: String, job_title: String) : Boolean
     createUser(name: String, email: String, job_title: String) : Boolean
     deleteUser(id: Int) : Boolean
@@ -75,56 +83,129 @@ const queryDB = (req, sql, args) => new Promise((resolve, reject) => {
     req.mysqlDb.query(sql, args, (err, rows) => {
         if (err)
             return reject(err);
-        console.log(rows);
-        rows.changedRows || rows.affectedRows || rows.insertId ? resolve(rows) : resolve(rows);
+        //console.log(rows);
+        if(rows.insertId){
+          resolve(rows.insertId)
+        }else{
+          resolve(rows)
+        }
+        //rows.changedRows || rows.affectedRows || rows.insertId ? resolve(rows) : resolve(rows);
     });
 });
 
+function generarToken(){
+  return Math.random().toString(36).substr(2);
+
+}
+
+function validarToken(req, token){
+  queryDB(req, "select * from token where token = '" + token + "' and vigente = 1 ")
+  .then((data) => {
+    console.log("data en funcion: ", data);
+    if(data.length > 0){
+      console.log("Entre al if");
+      return true;
+    }else{
+      console.log("Entre al else");
+      return false;
+    }
+  })
+
+}
+
 const root = {
 
-  productos: (args, req) => queryDB(req, "select * from producto").then((data) => {console.log(data); return data;}),
-  addProducto: (args, req) => queryDB(req, "insert into producto SET ?", args).then((data) => {console.log(data); return data;}),
-  clientes: (args, req) => queryDB(req, "select * from cliente").then(data => data),
-  addCliente: (args, req) => queryDB(req, "insert into cliente SET ?", args).then((data) => {console.log(data); return data;}),
+  //QUERYS
+
+  productos: (args, req) => queryDB(req, "select * from producto").then((data) => {console.log("Productos: ", data); return data;}),
+  clientes: (args, req) => queryDB(req, "select * from cliente").then((data) => {console.log("Clientes", data); return data;}),
+  categorias: (args, req) => queryDB(req, "select * from categoria").then((data) => {console.log("Categorias: ", data); return data;}),
+  canjes: (args, req) => queryDB(req, "select * from canje").then((data) => {console.log("Canjes: ", data); return data;}),
+
+  //MUTATIONS
 
   login: (args, req) => {
-     queryDB(req, "select * from cliente where mail = ? and password = ?", [args.mail, args.password])
-     .then((data) => {
-       if(data.length > 0){
-        //Generar token
-       }
-       console.log(data);
-        return data;
-      })
-    },
-  categorias: (args, req) => queryDB(req, "select * from categorias").then(data => data),
+    queryDB(req, "select * from cliente where mail = ? and password = ?", [args.mail, args.password])
+    .then((data) => {
+      if(data.length > 0){
+       let token = generarToken();
+       console.log("Token", token);
+       queryDB(req, "insert into token values('', " + data[0].ClientID + ", 1, '" + token + "')").then((data) => {console.log("Data Token: ", data); return data;});
+       console.log("Login data: ", data);
+       const productos = queryDB(req, "select * from producto").then((data) => {console.log("productos data: ", data); return data;})
+        
+       return data;
+      } else{
+        console.log("array vacio, data vacia");
+      }
+     //Else, devolver error
+      
+     })
+  },
+  addProducto: (args, req) => queryDB(req, "insert into producto SET ?", args).then((data) => {console.log(data); return data;}),
+  addCliente: (args, req) => queryDB(req, "insert into cliente SET ?", args).then((data) => {console.log(data); return data;}),
+  addCategoria: (args, req) => {
+    queryDB(req, "insert into categoria SET ?", args)
+    //.then(data => data);
+    .then((data) => {
+      const cat = JSON.parse(JSON.stringify(data));
+      return cat.insertId;
+    }).then((res) => {
+      queryDB(req, "select * from categoria where categoriaID = " + cat.insertId)
 
-    addCategoria: (args, req) => {
-      queryDB(req, "insert into categoria SET ?", args)
-      //.then(data => data);
-  
-  
       .then((data) => {
-        const cat = JSON.parse(JSON.stringify(data));
-        return cat.insertId;
-      }).then((res) => {
-        queryDB(req, "select * from categoria where categoriaID = " + cat.insertId)
-  
-        .then((data) => {
-  
-          //const result = Object.values(JSON.parse(JSON.stringify(data[0])));
-          //console.log('result', result);
-          console.log('data', JSON.parse(JSON.stringify(data)));
-          console.log('data[0]', JSON.parse(JSON.stringify(data[0])));
-          const result = JSON.parse(JSON.stringify(data[0]));
-          return {result};
-  
-          //console.log('data[0][0]: ', data[0][0]);
-          //return data[0][0];
-        })
-  
+
+        //const result = Object.values(JSON.parse(JSON.stringify(data[0])));
+        //console.log('result', result);
+        console.log('data', JSON.parse(JSON.stringify(data)));
+        console.log('data[0]', JSON.parse(JSON.stringify(data[0])));
+        const result = JSON.parse(JSON.stringify(data[0]));
+        return {result};
+
+        //console.log('data[0][0]: ', data[0][0]);
+        //return data[0][0];
       })
-    },
+
+    })
+  },
+  addCanje: (args, req) => {
+    //console.log("argumentos: ", args)
+
+    
+    /*const tokenValido = validarToken(req, args.token);
+    if(!tokenValido){
+      console.log("token invalido")
+      return "Token invalido";
+    }*/
+
+    delete args["token"];
+    queryDB(req, "insert into canje SET ?", args)
+    .then((data) => {
+      if(data){
+       console.log("Add Canje: ", data);
+       //const cliente = queryDB(req, "update cliente SET puntos = 1 where ClientID = ?", args.clientID)
+       const canjes = queryDB(req, "select * from canje")
+       .then((data) => {
+         console.log("Canje data: ", data); 
+         return data;
+        });
+       return canjes;
+      } else{
+        console.log("array vacio, data vacia", data);
+      }
+     //Else, devolver error
+    }).catch(e => {
+      console.log(e);
+    })
+  },
+  addCanjeProducto: (args, req) => queryDB(req, "insert into canjeproducto SET ?", args)
+  .then((data) => {
+    console.log(data);
+     return data;
+  })
+  //const puntos = queryDB(req, "insert into canjeproducto SET ?", args)
+
+  
 
   /*getUsers: (args, req) => queryDB(req, "select * from users").then(data => data),
   getUserInfo: (args, req) => queryDB(req, "select * from users where id = ?", [args.id]).then(data => data[0]),
